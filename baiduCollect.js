896a7683e091827dc2365e92a249bae83710f1f1
sinpo0 <Sinpo96@163.com>
2020-09-30 12:22:58 +0800

    feat：添加百度统计相关的不知道是什么东西的东西...

---
+++ baiduCollect.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?ed56d21c11de0a56f176f97c37c5c861";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
